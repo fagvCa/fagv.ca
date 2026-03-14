e27a174ece321cfa1c930ca9df7489b549ac421a
GomezFabio <fagv.ca@gmail.com>
2026-03-14 01:47:23 -0400

    fix rebase conflict

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  output: "export",
-=======
   /* config options here */
   output: "export",
   distDir: "build",
@@ -11,7 +8,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
->>>>>>> 247dc3d (feat: Migrate to `next/image` for displaying new image assets, refine AI model references, and update build configuration for static export.)
 };
 
 export default nextConfig;
